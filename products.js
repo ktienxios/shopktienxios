db90f2e321104e844d906218967dc9c136f20394
ktienxios <huaquangvinh503@gmail.com>
2026-05-01 16:50:56 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,24 @@
+// products.js
+
+// --- CẤU HÌNH HỆ THỐNG ---
+const KTIENXIOS_CONFIG = {
+    musicUrl: "https://scared-aquamarine-wzjsbpfpzz.edgeone.app/snaptt.me-48765111.mp3",
+    musicTitle: "BGM - KTIENXIOS SYSTEM",
+    autoPlayOnTouch: true // Kích hoạt chế độ chạm để phát nhạc
+};
+
+const KTIENXIOS_PRODUCTS = [
+    {
+        id: 1,
+        name: "IPA BYPASS V3 NEW",
+        img: "https://cdn.phototourl.com/member/2026-05-01-8ac76032-e981-4768-9d91-427baa3b16b8.jpg",
+        desc: "Hỗ trợ Full chức năng, nghe gọi cho mọi dòng iPhone/iPad.",
+        hot: true,
+        options: [
+            { label: "Gói Ngày", price: "5.000đ" },
+            { label: "Gói Tuần", price: "20.000đ" },
+            { label: "Gói Tháng", price: "50.000đ" }
+        ]
+        
+    }
+];
