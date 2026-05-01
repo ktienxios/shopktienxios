4565373c99a8cdb88fb4d28aaa7760181e608db1
ktienxios <huaquangvinh503@gmail.com>
2026-05-01 22:58:12 +0700

    Update products.js

--- products.js
+++ products.js
@@ -19,6 +19,14 @@ const KTIENXIOS_PRODUCTS = [
             { label: "Gói Tuần", price: "20.000đ" },
             { label: "Gói Tháng", price: "50.000đ" }
         ]
-        
+    },
+    {
+        id: 2, // ID tiếp theo là 2
+        name: "ACC CLONE FREE FIRE",
+        img: "https://cdn.phototourl.com/member/2026-05-01-f9052ff4-5ae0-4d37-af5e-98ab57098349.gif", // Ông có thể thay link ảnh tùy ý
+        desc: "Acc clone trắng thông tin, ngâm lâu, cực kỳ ổn định.",
+        hot: true,
+        price: 4000, // Giá 4.000đ một acc
+        isClone: true // Đánh dấu để xử lý nhập số lượng ở file index
     }
 ];
